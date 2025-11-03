f431d74cc5cfe7ee984c7e4ed12ca6c3b2e8a66f
Ravi Pandey <118262813+ravi1215@users.noreply.github.com>
2025-11-03 20:49:23 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import QRCode from 'qrcode';
 dotenv.config()
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 // connecion to database
